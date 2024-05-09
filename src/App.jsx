a7ac6e481cc88e68ae6dad949553a5245828ad26
parkjh <ace2006jh@naver.com>
2024-05-09 14:43:48 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-function App() {
-  let a; // 값이 할당되지 않으면 undefined
-  console.log("a", typeof a);
-  let b = "문자열 타입"; // string
-  console.log("b", typeof b);
-  let c = 3; //number
-  console.log("c", typeof c);
-  let d = 3.14; //number
-  console.log("d", typeof d);
-  let e = true; // boolean
-  console.log("e", typeof e);
-  let f = {}; //object
-  console.log("f", typeof f);
+import React from "react";
 
-  let g = function () {}; // function
-  console.log("g", typeof g);
-
-  return null;
+function App(props) {
+  return <div></div>;
 }
 
 export default App;
