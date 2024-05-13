671bdec93501ae7f104b948cd5ebf9d6c59d6cfc
parkjh <ace2006jh@naver.com>
2024-05-13 14:15:29 +0900

    + - 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyNumber() {
+  const [number, setNumber] = useState(1000);
+  return (
+    <div>
+      <div>{number}</div>
+
+      <div>
+        <button onClick={() => setNumber(number - 1)}></button>
+        <button onClick={() => setNumber(number + 1)}></button>
+      </div>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyNumber />
+    </div>
+  );
 }
 
 export default App;
